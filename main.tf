8000a626ecd2b2587254252b99523caaa8d59041
Nicolas Harlem Eide <nicolas@harlemeide.net>
2022-01-19 13:27:03 +0100

    Add back RHEL RPM repository activations
    These were activated on the hosts I tested on,
    so I didn't notice that they were actually required.

--- main.tf
+++ main.tf
@@ -111,6 +111,7 @@ resource "null_resource" "configure_instance" {
 
       # Get dependencies ready
       # This assumes that we're running on RHEL 7 (which Vy hosts are).
+      "echo ${each.value.password} | sudo -S -k subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-extras-rpms --enable=rhel-7-server-optional-rpms",
       "echo ${each.value.password} | sudo -S -k yum install -y yum-utils device-mapper-persistent-data lvm2",
       "echo ${each.value.password} | sudo -S -k yum install -y https://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.107-3.el7.noarch.rpm",
       "echo ${each.value.password} | sudo -S -k yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
